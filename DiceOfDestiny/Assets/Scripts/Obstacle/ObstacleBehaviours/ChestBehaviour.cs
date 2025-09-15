4baad11a494089f0e4f1a5d38b9d626dfe69a60d
leeworm <kalrocse@gmail.com>
2025-09-15 16:15:42 +0900

    Feat: Toast Message of Chest

--- DiceOfDestiny/Assets/Scripts/Obstacle/ObstacleBehaviours/ChestBehaviour.cs
+++ DiceOfDestiny/Assets/Scripts/Obstacle/ObstacleBehaviours/ChestBehaviour.cs
@@ -14,13 +14,14 @@ public class ChestBehaviour : Obstacle, IObstacleBehaviour
             if (className == "Thief")
             {
                 Debug.Log("도둑이 상자를 엽니다.");
+
                 RuleEvents.TriggerRule("Thief_Passive_OpenBox");
 
                 SkillManager.Instance.ThiefPassive();
                 
                 string item = InventoryManager.Instance.GetRandomItem();
 
-                ToastManager.Instance.ShowToast(item, this.transform, 1f);
+                ToastManager.Instance.ShowToast($"{item}을(를) 얻었습니다.", this.transform, 1.5f);
 
                 BoardManager.Instance.RemoveObstacle(this);
 
@@ -28,6 +29,7 @@ public class ChestBehaviour : Obstacle, IObstacleBehaviour
             }
 
             Debug.Log("상자는 도둑만 열 수 있습니다.");
+            ToastManager.Instance.ShowToast("상자는 도둑만 열 수 있습니다.", this.transform, 1f);
         }
     }
 }
